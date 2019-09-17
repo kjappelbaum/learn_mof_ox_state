e8b5c57b4d300f8dff793eece74987e0ba376cd6
kjappelbaum <kevin.jablonka@epfl.ch>
2019-09-17 12:52:50 +0200

    initializing dvc, adding data

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ REQUIRES_PYTHON = '>=3.6.0'
 VERSION = '0.1.0a1'
 
 # What packages are required for this module to be executed?
-REQUIRED = ['sklearn', 'mlxtend', 'pandas', 'click']
+REQUIRED = ['sklearn', 'mlxtend', 'pandas', 'click', 'comet', 'dvc']
 
 # What packages are optional?
 EXTRAS = {
